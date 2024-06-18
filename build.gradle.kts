4638fbba7ac07939125b98ccd0e8a87515e9c69a
starry-shivam <starry@krsh.dev>
2024-06-18 12:07:18 +0530

    ci: Add test coverage badge and action
    Signed-off-by: starry-shivam <starry@krsh.dev>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import javax.xml.parsers.DocumentBuilderFactory
+
 plugins {
     kotlin("jvm") version "2.0.0"
     id("org.jetbrains.kotlinx.kover") version "0.8.1"
@@ -24,7 +26,8 @@ dependencies {
 
 kover {
     reports {
-        verify { rule { minBound(70) } }
+        // Require 95% minimum test coverage.
+        verify { rule { minBound(95) } }
     }
 }
 
@@ -36,6 +39,41 @@ publishing {
             version = version.toString()
             from(components["java"])
         }
+    }
+}
+
+// Print line coverage percentage to console so we can generate badge in CI.
+tasks.register("printLineCoverage") {
+    group = "verification"
+    dependsOn("koverXmlReport")
+    doLast {
+        val report = file("$buildDir/reports/kover/report.xml")
+
+        val doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(report)
+        val rootNode = doc.firstChild
+        var childNode = rootNode.firstChild
+
+        var coveragePercent = 0.0
+
+        while (childNode != null) {
+            if (childNode.nodeName == "counter") {
+                val typeAttr = childNode.attributes.getNamedItem("type")
+                if (typeAttr.textContent == "LINE") {
+                    val missedAttr = childNode.attributes.getNamedItem("missed")
+                    val coveredAttr = childNode.attributes.getNamedItem("covered")
+
+                    val missed = missedAttr.textContent.toLong()
+                    val covered = coveredAttr.textContent.toLong()
+
+                    coveragePercent = (covered * 100.0) / (missed + covered)
+
+                    break
+                }
+            }
+            childNode = childNode.nextSibling
+        }
+
+        println("%.1f".format(coveragePercent))
     }
 }
 
